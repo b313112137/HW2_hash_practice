9b1ab81d1ef84adeba3b8bb06d835667d1e87a4f
b313112137 <b313112137@tmu.edu.tw>
2025-04-05 22:21:11 +0800

    Update main.py

--- Documents/HW2_hash_practice/main.py
+++ Documents/HW2_hash_practice/main.py
@@ -10,14 +10,14 @@ with open("hw2_data.txt", "r", encoding="utf-8") as file:
         else:
             word_count[word] = 1
 
-# 印出不重複單字數量
+
 print(f"不重複的單字數量：{len(word_count)}\n")
 
-# 印出每個單字的出現次數
+
 for word, count in word_count.items():
     print(f"{word}: {count}")
 
-# 排序後畫圖（由高到低）
+
 sorted_items = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 words = [item[0] for item in sorted_items]
 counts = [item[1] for item in sorted_items]
